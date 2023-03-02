34a76b267f9683b1876972e401573d89271c13de
root <shwethakm1331@gmail.com>
2023-03-02 08:36:03 +0000

    prime video

---
+++ src/components/PrimeVideo/index.js
@@ -0,0 +1,35 @@
+// Write your code here
+import MoviesSlider from '../MoviesSlider'
+// import MovieItem from '../MovieItem'
+
+import './index.css'
+
+const Primevideo = props => {
+  const {moviesList} = props
+
+  const actionMoviesList = moviesList.filter(
+    eachMovie => eachMovie.categoryId === 'ACTION',
+  )
+
+  const comedyMoviesList = moviesList.filter(
+    eachMovie => eachMovie.categoryId === 'COMEDY',
+  )
+
+  return (
+    <div className="app-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/prime-video-img.png"
+        alt="prime video"
+        className="prime-image"
+      />
+      <div className="movies-container">
+        <h1 className="heading">Action Movies</h1>
+        <MoviesSlider moviesList={actionMoviesList} />
+        <h1 className="heading">Comedy Movies</h1>
+        <MoviesSlider moviesList={comedyMoviesList} />
+      </div>
+    </div>
+  )
+}
+
+export default Primevideo
